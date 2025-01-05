ce8308c912e87f823944f18b4b9960ff42c3d2f8
Adriano Oliveira <8104069+adri0@users.noreply.github.com>
2025-01-05 15:44:03 +0100

    Update example pipeline

--- examples/my_data_pipeline/pipeline.py
+++ examples/my_data_pipeline/pipeline.py
@@ -7,6 +7,9 @@ from pydantic import BaseModel
 from pipestruct import PipelineDefinition, Transformation, initialize_pipeline
 
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Define your transaformations by inheriting the Transformation class.  #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 class FilterNumInterval(Transformation):
     column: str
     max_value: float | int = float("+Inf")
@@ -26,27 +29,36 @@ class FilterByValue(Transformation):
         return df[df[self.column] == self.value]
 
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Optionally you may create a pydantic model using PipelineDefinition   #
+# so that you have express your pipeline in a nice-looking config file. #
+# See definition.yml                                                    #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 class MyPipeline(BaseModel):
     pipeline: PipelineDefinition
 
 
-def run() -> None:
+def main() -> None:
+    # Parse the config file using your PipelineDefinition object
     with (Path(__file__).parent / "definition.yml").open("r") as f:
         my_data_pipe_def = MyPipeline.model_validate(yaml.safe_load(f))
 
+    # Initialise your pipeline by providing the PipelineDefinition
+    # and providing the transformations you defined
     pipeline = initialize_pipeline(
         my_data_pipe_def.pipeline,
         available_transformations=[FilterNumInterval, FilterByValue],
     )
 
+    # Load your data
     path_data = Path(__file__).parent / "data.csv"
-
     df = read_csv(path_data)
-    output = pipeline.apply(df)
 
+    # Run the pipeline
+    output = pipeline.apply(df)
     print("Processed df:")
     print(output)
 
 
 if __name__ == "__main__":
-    run()
+    main()
